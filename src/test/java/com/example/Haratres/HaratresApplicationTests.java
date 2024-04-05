f86d75cce5f30a2e7dd4f403893e5fa85301ca81
Duygu <duyguasenakaya@gmail.com>
2024-04-05 13:22:31 +0300

    init project

---
+++ src/test/java/com/example/Haratres/HaratresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Haratres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaratresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
